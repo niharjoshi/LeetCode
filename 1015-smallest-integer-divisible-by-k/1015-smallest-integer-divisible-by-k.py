61dff24bf7f39dbaef24bc9dc27d1ac5bce72197
Nihar Joshi <nsj0596@gmail.com>
2022-02-27 19:13:02 -0600

    Time: 1984 ms (27.81%), Space: 19.3 MB (5.22%) - LeetHub

---
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
@@ -0,0 +1,39 @@
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        """
+        k -> +ve int
+        n -> smallest +ve int such that n % k == 0, only contains 1s
+        ans -> len(n) or -1
+        
+        Examples:
+        
+        k = 3, n = (1, 1), (11, 2), (111, 0)
+        k = 7, n = (1, 1), (11, 4), (111, 6), (1111, 5), (11111, 2), (111111, 0), (1111111, 1), (11111111, 4)
+        """
+        
+        if k % 2 == 0 or k % 5 == 0:
+            return -1
+        
+        n = 1
+        n_counter = 1
+        
+        repeated = {}
+        
+        remainder = -1
+        
+        for i in range(k):
+            
+            remainder = n % k
+            
+            if remainder == 0:
+                return n_counter
+            
+            if remainder in repeated:
+                return -1
+            else:
+                repeated[remainder] = 0
+            
+            n = n * 10 + 1
+            n_counter += 1
+        
+        return -1
