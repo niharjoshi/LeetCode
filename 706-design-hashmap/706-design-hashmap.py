a28e7185ca70fd848e5417b7fe7a05bc43e2359a
Nihar Joshi <nsj0596@gmail.com>
2022-03-03 16:32:02 -0600

    Time: 371 ms (57.21%), Space: 17.5 MB (53.96%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.py
+++ 706-design-hashmap/706-design-hashmap.py
@@ -1,20 +1,55 @@
+class ListNode:
+    
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+
 class MyHashMap:
 
     def __init__(self):
-        self.hashmap = dict()
+        self.size = 1000
+        self.hash_table = [None] * self.size
         
     def put(self, key: int, value: int) -> None:
-        self.hashmap[key] = value
+        index = key % self.size
+        if self.hash_table[index] == None:
+            self.hash_table[index] = ListNode(key, value)
+        else:
+            curr = self.hash_table[index]
+            while True:
+                if curr.key == key:
+                    curr.val = value
+                    return
+                if curr.next == None:
+                    break
+                curr = curr.next
+            curr.next = ListNode(key, value)
+                    
         
     def get(self, key: int) -> int:
-        if key in self.hashmap:
-            return self.hashmap[key]
-        else:
-            return -1
+        index = key % self.size
+        curr = self.hash_table[index]
+        while curr != None:
+            if curr.key == key:
+                return curr.val
+            curr = curr.next
+        return -1
         
     def remove(self, key: int) -> None:
-        if key in self.hashmap:
-            del self.hashmap[key]
+        index = key % self.size
+        curr = prev = self.hash_table[index]
+        if not curr:
+            return
+        if curr.key == key:
+            self.hash_table[index] = curr.next
+        else:
+            curr = curr.next
+            while curr:
+                if curr.key == key:
+                    prev.next = curr.next
+                    break
+                prev, curr = prev.next, curr.next
             
 
 # Your MyHashMap object will be instantiated and called as such:
